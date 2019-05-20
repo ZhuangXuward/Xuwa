fcbbc60ca6755bd0f1d8afce313f92272282cd55
ZhuangXuward <1032003118@qq.com>
2019-05-20 23:16:04 +0800

    修改demo页，添加滑动窗口

---
+++ projects/content.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+	var slideClick = document.getElementsByClassName("slideClick");
+	var slideUP = document.getElementsByClassName("slideUP");
+	for (let i = 0; i < slideClick.length; ++ i) {
+		let totalCount = slideUP[i].getElementsByTagName("a").length;
+		slideClick[i].getElementsByTagName("a")[0].innerHTML += "(" + totalCount + ")";
+		slideClick[i].onclick = function() {
+			$("div#block" + (i + 1)).slideToggle();
+			clickHide(i, slideUP.length);
+		};
+	}
+}
+
+function clickHide(index, total) {
+	var slideUP = document.getElementsByClassName("slideUP");
+	for (let j = 0; j < total; ++ j) {
+		if (j == index) continue;
+		$("div#block" + (j + 1)).slideUp();
+	}
+}
